7a06250bf0a59691d7551acb425ebefd1f82856f
kingstar <admin@baks.dev>
2024-04-19 22:56:00 +0300

    Baks Development

---
+++ Email/Resources/config/service.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use BaksDev\Field\Pack\Phone\Choice\PhoneFieldChoice;
+
+return static function(ContainerConfigurator $configurator) {
+
+    $services = $configurator->services()
+        ->defaults()
+        ->autowire(true)
+        ->autoconfigure(true);
+
+
+    $NAMESPACE = 'BaksDev\Field\Pack\Email\\';
+
+    $MODULE = substr(__DIR__, 0, strpos(__DIR__, "Resources"));
+
+    $services->load($NAMESPACE.'Form\\', $MODULE.'Form');
+
+    $services->load($NAMESPACE.'Twig\\', $MODULE.'Twig');
+
+    $services->set(PhoneFieldChoice::class)
+        ->tag('baks.fields.choice');
+
+};
+
